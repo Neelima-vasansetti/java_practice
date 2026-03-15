fa8ad8737ccf73c95f969427e5c9e7672de47918
Neelima-vasansetti <neelimavasanasetti88@gmail.com>
2026-03-15 22:49:49 +0530

    added arrayDemo.java file

---
+++ arrayDemo.java
@@ -0,0 +1,8 @@
+public class arrayDemo{
+    public static void main(String[] argss){
+        int arr[] = {1,2,3,4,5};
+        String [] names = {"neelu","hello","world"};
+        System.out.println(arr[0]);
+        System.out.println(names[0]);
+    }
+}
