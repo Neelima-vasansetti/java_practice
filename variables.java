0d4b04de3c2ba408062c54df94d97a7414c8bd08
Neelima-vasansetti <neelimavasanasetti88@gmail.com>
2026-03-16 18:34:06 +0530

    added variables.java file

---
+++ variables.java
@@ -0,0 +1,21 @@
+public class variables {
+    public static void main(String[] args){
+        String name ="neelu";
+        int age = 22;
+        int marks = 95;
+        System.out.println("name: " + name);
+        System.out.println("age: " + age);
+        System.out.println("marks: " + marks);
+
+        double length =8.8;
+        double width = 4.5;
+        double area = length * width;
+        System.out.println("area: " + area);
+
+        float price = 19.99f;
+        int quantity = 5;
+        float totalCost = price * quantity;
+        System.out.println("total cost: " + totalCost);
+    
+    }
+}
