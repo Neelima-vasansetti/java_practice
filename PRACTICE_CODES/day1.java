e2d7f5316d80a3773e9bd9cf8c3c9f91c7879139
Neelima-vasansetti <neelimavasanasetti88@gmail.com>
2026-03-16 22:48:20 +0530

    added negative or positive code file

---
+++ PRACTICE_CODES/day1.java
@@ -0,0 +1,19 @@
+package PRACTICE_CODES;
+
+public class day1
+{
+    public static void main(String[] args)
+    {
+        int num = 8;
+
+        if (num > 0) {
+            System.out.println("The number is positive.");
+        } 
+        else if (num < 0) {
+            System.out.println("The number is negative.");
+        } 
+        else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
