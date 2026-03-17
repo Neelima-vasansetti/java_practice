4a5f104d67bf8b0b040d87ad3608b2e6fb4dbd8c
Neelima-vasansetti <neelimavasanasetti88@gmail.com>
2026-03-17 23:08:23 +0530

    added even odd code

---
+++ PRACTICE_CODES/day2.java
@@ -0,0 +1,12 @@
+package PRACTICE_CODES;
+
+public class day2{
+    public static void main(String[] args)
+    {
+        int n = 8;
+        if (n % 2 == 0)
+            System.out.println("The number is even.");
+        else
+            System.out.println("The number is odd.");            
+    }
+}
