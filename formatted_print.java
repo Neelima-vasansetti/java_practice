5d71a5e330822a7c888f8865462554e73787d42d
Neelima-vasansetti <neelimavasanasetti88@gmail.com>
2026-03-19 21:56:12 +0530

    added formatted_print.file

---
+++ formatted_print.java
@@ -0,0 +1,12 @@
+public class formatted_print{
+    public static void main(String[] args){
+        String name = "NEELIMA VASANASETTI";
+        int age = 21;
+        boolean isStudent = true;
+        double hieght = 5.5;
+        System.out.println("Name:%s" + name);
+        System.out.println("Age:%d" + age);
+        System.out.println("isStudent:%b" + isStudent);
+        System.out.println("Hieght:%.2f" + hieght);
+    }
+}
