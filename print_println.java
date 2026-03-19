8cd61da5683b07b92ba3033f220418db61a72785
Neelima-vasansetti <neelimavasanasetti88@gmail.com>
2026-03-19 21:32:19 +0530

    added print_println.java files

---
+++ print_println.java
@@ -0,0 +1,11 @@
+//difference between print and println
+public class print_println {
+    public static void main(String[] args){
+        System.out.print("Hello ");
+        System.out.println("NEELU");
+        System.out.println("are you neelima?");
+        System.out.print("yes");
+        System.out.print(" I am neelima");
+
+    }
+}
