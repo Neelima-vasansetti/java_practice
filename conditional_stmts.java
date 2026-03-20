2286644b4f99e169c26805f4783c568b632c6e65
Neelima-vasansetti <neelimavasanasetti88@gmail.com>
2026-03-20 23:32:35 +0530

    added conditional_stmts.java file

---
+++ conditional_stmts.java
@@ -0,0 +1,10 @@
+public class conditional_stmts
+{
+    public static void main(String[] args)
+    {
+        int i = 8;
+        if (i > 4){
+            System.out.println("condition is true");
+        }
+    }
+}
