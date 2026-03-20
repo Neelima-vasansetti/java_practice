c4fdddcb3ade11f33ec83e6e49aa6d7f1791353e
Neelima-vasansetti <neelimavasanasetti88@gmail.com>
2026-03-20 23:42:32 +0530

    added conditional.java file

---
+++ conditional.java
@@ -0,0 +1,15 @@
+import java .util.Scanner;
+public class conditional {
+    public static void main(String[] args)
+    {
+        Scanner src = new Scanner(System.in);
+        int n = src.nextInt();
+        if (n>8)
+            System.out.println("n is greater than 8");
+        else
+            System.out.println("n is less than 8");
+       
+
+    }
+
+}
