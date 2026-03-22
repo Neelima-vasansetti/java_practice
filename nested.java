c3f0b9709863fa2de29e4d5bcd23c9fa4e2863d9
Neelima-vasansetti <neelimavasanasetti88@gmail.com>
2026-03-22 22:34:14 +0530

    added nested.java file

---
+++ nested.java
@@ -0,0 +1,12 @@
+class nested {
+    public static void main(String[] args){
+        int n = 8;
+        if (n>10){
+        System.out.println("Greater than 10");
+            if (n==8){
+                System.out.println("Exactly 8");
+            }
+        }
+    }
+}
+
